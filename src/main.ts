c9d2ea1af8706dd07f6aa8aa2086c3532f57ea3a
JoseR200 <joserodrigolopez@icloud.com>
2025-07-31 10:49:20 +0200

    Fix swagger problem

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,10 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  if (process.env.NODE_ENV === 'production') {
+    app.setGlobalPrefix('qr');
+  }
+
   const config = new DocumentBuilder()
     .setTitle('Qr-Backend')
     .addBearerAuth(
