bc462c5cdc380ff1dfd52b0753a0d3f9345f81e3
JoseR200 <joserodrigolopez@icloud.com>
2025-08-01 17:06:03 +0200

    Validators

--- src/core/order/dto/create-order.dto.ts
+++ src/core/order/dto/create-order.dto.ts
@@ -1,4 +1,4 @@
-import { IsArray, IsEnum, IsNumber, IsOptional, ValidateNested } from 'class-validator';
+import { ArrayNotEmpty, IsArray, IsEnum, IsNumber, IsOptional, ValidateNested } from 'class-validator';
 import { ApiProperty } from '@nestjs/swagger';
 import { Type } from 'class-transformer';
 import { CreateOrderItemDto } from './create-order-item.dto';
@@ -6,21 +6,24 @@ import { OrderStatus } from '../entity/order.entity';
 
 export class CreateOrderDto {
   @IsOptional()
+  @ApiProperty({ example: 'string' })
   description?: string;
 
   @IsNumber({}, { message: 'El identificador de la mesa debe ser un número.' })
+  @ApiProperty({ example: 1 })
   tableNumber: number;
 
   @IsEnum(OrderStatus, { message: 'El estado enviado es incorrecto.' })
-  @ApiProperty({ enum: OrderStatus, example: OrderStatus.CREADO })
+  @ApiProperty({ enum: OrderStatus, example: OrderStatus.RECIBIDO })
   status: OrderStatus;
 
   @IsNumber({}, { message: 'El ID de la sede debe ser un número.' })
   @ApiProperty({ example: 1 })
   branchId: number;
 
-  @IsArray()
-  @ValidateNested({ each: true })
+  @IsArray({ message: 'Los items deben ser una lista.' })
+  @ArrayNotEmpty({ message: 'Los items no pueden estar vacíos.' })
+  @ValidateNested({ each: true, message: 'Cada item debe ser válido.' })
   @Type(() => CreateOrderItemDto)
   @ApiProperty({
     example: [
