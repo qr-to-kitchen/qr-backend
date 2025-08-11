2fabd5de85e93218a80ea7857171ec15c5ffdc32
JoseR200 <joserodrigolopez@icloud.com>
2025-08-11 20:11:13 +0200

    Uploades version

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,15 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+} from '@nestjs/common';
 
 @Controller()
-export class AppController {}
+export class AppController {
+
+  @Get('version')
+  getVersion() {
+    return {
+      version: '1.0.0'
+    };
+  }
+}
